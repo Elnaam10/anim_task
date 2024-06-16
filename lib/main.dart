48f0b7e404b78e3542a30b8face161d3c06b8521
Elnaam10 <e.umutoni@alustudent.com>
2024-06-16 15:36:06 +0200

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Opacity Animation',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
       home: OpacityAnimationDemo(),
     );
